e34a3edb2a8baa7cee013e0dcb348ff0564c66b7
icodejsx <72747787+icodejsx@users.noreply.github.com>
2025-01-26 00:40:35 +0100

    working

--- app/components/CTA.tsx
+++ app/components/CTA.tsx
@@ -1,28 +1,58 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { BsHouseDoorFill } from "react-icons/bs";
 import { FaFacebook } from "react-icons/fa";
+import { GoArrowRight } from "react-icons/go";
 
 const CTA = () => {
   return (
-    <section className="lg:grid grid-cols-2 mt-10 md:mt-24 flex-col container ">
-      <main className=" col-span-1 flex flex-col items-center lg:items-start  lg:justify-between ">
-        <div className=" flex items-start py-2 lg:py-0  ">
-          <div className="flex flex-row items-center text gap-2 font-bold p-2 rounded-lg bg-[#056FC533]    ">
-            <BsHouseDoorFill className="text-[#056FC5] text-xl" />
-            <p className="text-[#056FC5] ]">Who we are</p>
+    <section className="flex flex-col gap-3">
+      {/* first section */}
+      <div className="lg:grid grid-cols-2 mt-10 md:mt-24 flex-col container ">
+        <main className=" col-span-1 flex flex-col items-center lg:items-start  lg:justify-between ">
+          <div className=" flex items-start py-2 lg:py-0  ">
+            <div className="flex flex-row items-center text gap-2 font-bold p-2 rounded-lg bg-[#056FC533]    ">
+              <BsHouseDoorFill className="text-[#056FC5] text-xl" />
+              <p className="text-[#056FC5] ]">Who we are</p>
+            </div>
           </div>
-        </div>
-        <p className="lg:text-2xl md:text-xl text-justify text-[#323232] font-medium">
-          At VY Construction, we understand that it&apos;s important for your
-          home to look its best. That&apos;s why our team consists of
-          experienced professionals who are committed to providing quality
-          services. We strive to make sure that your home is attractive and
-          well-maintained, with exterior repairs, deck repairs, new deck builds,
-          siding and gutter guard installation.
-        </p>
+          <p className="lg:text-2xl md:text-xl text-justify text-[#323232] font-medium">
+            At VY Construction, we understand that it&apos;s important for your
+            home to look its best. That&apos;s why our team consists of
+            experienced professionals who are committed to providing quality
+            services. We strive to make sure that your home is attractive and
+            well-maintained, with exterior repairs, deck repairs, new deck
+            builds, siding and gutter guard installation.
+          </p>
 
-        <div className="col-span-1 flex justify-end lg:hidden py-10">
+          <div className="col-span-1 flex justify-end lg:hidden py-10">
+            <Image
+              src={"/house1.png"}
+              alt="hero"
+              width={500}
+              height={300}
+              quality={100}
+              className="object-cover"
+            />
+          </div>
+
+          <div className="lg:text-2xl md:text-xl text-[#032759] font-medium">
+            <p className="font-bold">We Offer:</p>
+            <p className="space-x-20">
+              Wood Rot Repair • Termite Damage Repair • Deck Repairs • New Deck
+              Builds • Siding • Pergolas and Patio Covers • Gutter Cleaning •
+              Gutter Guard Installation
+            </p>
+          </div>
+
+          <div className="flex flex-row items-center gap-4 py-5 md:py-0 lg:text-xl">
+            <p className="border-2  p-0.5 bg-[#056FC5] w-16"></p>
+            <p className="text-[#056FC5] ">Follow our social</p>
+            <FaFacebook className="text-[#056FC5] text-2xl lg:text-4xl" />
+          </div>
+        </main>
+        <div className="col-span-1 lg:flex justify-end hidden">
           <Image
             src={"/house1.png"}
             alt="hero"
@@ -32,31 +62,59 @@ const CTA = () => {
             className="object-cover"
           />
         </div>
+      </div>
 
-        <div className="lg:text-2xl md:text-xl text-[#032759] font-medium">
-          <p className="font-bold">We Offer:</p>
-          <p className="space-x-20">
-            Wood Rot Repair • Termite Damage Repair • Deck Repairs • New Deck
-            Builds • Siding • Pergolas and Patio Covers • Gutter Cleaning •
-            Gutter Guard Installation
+      {/* section section */}
+
+      <div className="lg:grid grid-cols-2 mt-10 md:mt-24 flex-col container  ">
+        <div className="col-span-1 lg:flex justify-start hidden">
+          <Image
+            src={"/house2.png"}
+            alt="hero"
+            width={500}
+            height={300}
+            quality={100}
+            className="object-cover"
+          />
+        </div>
+        <main className=" col-span-1 flex flex-col items-center lg:items-start  lg:justify-between ">
+          <div className="lg:text-4xl text-2xl text-center bg-white md:p-3 py-3 md:py-0  rounded-md font-medium">
+            <p className="space-x-20">
+              We specialize in building pergolas and patio covers to give your
+              home{" "}
+              <span className="text-[#056FC5]">
+                a great outdoor living space.
+              </span>
+            </p>
+          </div>
+
+          <div className="col-span-1 flex justify-end lg:hidden py-10">
+            <Image
+              src={"/house2.png"}
+              alt="hero"
+              width={500}
+              height={300}
+              quality={100}
+              className="object-cover"
+            />
+          </div>
+
+          <p className="lg:text-2xl md:text-xl text-justify text-[#323232] font-medium">
+            With our experience and dedication to providing excellent services,
+            you can trust that your home will look its best with VY
+            Construction. Contact us today to learn more about our services and
+            how we can help you get the most out of your home. We look forward
+            to working with you!
           </p>
-        </div>
 
-        <div className="flex flex-row items-center gap-4 lg:text-xl">
-          <p className="border-2 p-1 bg-[#056FC5] w-16"></p>
-          <p className="text-[#056FC5] ">Follow our social</p>
-          <FaFacebook className="text-[#056FC5] text-2xl lg:text-4xl" />
-        </div>
-      </main>
-      <div className="col-span-1 lg:flex justify-end hidden">
-        <Image
-          src={"/house1.png"}
-          alt="hero"
-          width={500}
-          height={300}
-          quality={100}
-          className="object-cover"
-        />
+          <Link
+            href={"/aboutus"}
+            className="flex flex-row items-center gap-4 lg:text-xl"
+          >
+            <p className="text-[#056FC5] ">More About us </p>
+            <GoArrowRight className="text-[#056FC5] text-2xl lg:text-4xl" />
+          </Link>
+        </main>
       </div>
     </section>
   );
