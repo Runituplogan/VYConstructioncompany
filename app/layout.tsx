498cf157f537b08c25be797293c99d28003522ed
AdamJamiu <adamjamiu98@gmail.com>
2025-02-21 13:29:24 +0100

    embedded google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import Script from "next/script";
 import "aos/dist/aos.css"; // Import AOS CSS
 import "./globals.css";
 import "react-toastify/dist/ReactToastify.css";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "VY Construction",
@@ -29,20 +30,44 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-        {/* Google Analytics */}
-<Script async src="https://www.googletagmanager.com/gtag/js?id=G-MW1V59Z339"></Script>
-<Script
-  id="google-analytics"
-  dangerouslySetInnerHTML={{
-    __html: `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-MW1V59Z339');
-    `,
-  }}
-/>
+      {/* Google Analytics */}
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PSLBNBF7');
+            `,
+          }}
+        />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-MW1V59Z339"
+        ></Script>
+        <Script
+          id="google-analytics"
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-MW1V59Z339');
+          `,
+          }}
+        />
+      </Head>
       <body className={bricolage.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PSLBNBF7"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <ToastContainer
           autoClose={2000}
           hideProgressBar={true}
