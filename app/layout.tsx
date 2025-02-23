f9ff0aa6f2cfd28778605f8a76b982a587a90ba8
Faithful Chibuike <faithfulokondu@gmail.com>
2025-02-23 13:27:44 +0100

    Google analytics fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -143,7 +143,7 @@ export default function RootLayout({
           src="https://www.googletagmanager.com/gtag/js?id=G-MW1V59Z339"
           strategy="beforeInteractive"
         />
-        <Script
+        {/* <Script
           id="google-analytics"
           dangerouslySetInnerHTML={{
             __html: `
@@ -152,6 +152,20 @@ export default function RootLayout({
               gtag('js', new Date());
               gtag('config', 'G-MW1V59Z339');
             `,
+          }}
+          strategy="beforeInteractive"
+        /> */}
+
+        <Script
+          id="google-tag-manager"
+          dangerouslySetInnerHTML={{
+            __html: `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-PSLBNBF7');
+    `,
           }}
           strategy="beforeInteractive"
         />
